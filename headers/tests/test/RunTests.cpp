b3d38ac534ef7643069014c806c62e9f697e8e20
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 12:23:14 +0100

    Next try

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -1,7 +1,5 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
-    for (int i = 1; i < argc; ++i) {
-        std::cout << "Input" << i << "]: " << argv[i] << std::endl;
-    }
+    return RUN_ALL_TESTS();
 }
