9bd4a222f00f3791ee0ecaa2b77e90ffb689075f
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 12:19:39 +0100

    Next try

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
-    std::cout << "Input" << i << "]: " << argv[i] << std::endl;
+    for (int i = 1; i < argc; ++i) {
+        std::cout << "Input" << i << "]: " << argv[i] << std::endl;
+    }
 }
