40a1d7d40ce4b1339032b78a067ac3d5bba72bc2
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 12:52:07 +0100

    Revert changes

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    
+
+    std::cout << "TEST_DIR '" << TEST_DIR << "'" << std::endl;
+    for (int i = 1; i < argc; ++i) {
+        std::cout << "Input[" << i << "]: " << argv[i] << std::endl;
+    }
+
     return RUN_ALL_TESTS();
 }
