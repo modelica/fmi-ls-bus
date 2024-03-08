d44951c82f44df286df284e61eacf81812bc8c73
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 13:41:33 +0100

    Remove wrong character

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -3,7 +3,8 @@
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
-    std::cout << "Runnin tests in directory: " << TEST_DIR << "'" << std::endl;
+    std::cout << "Runnin tests in directory: " << TEST_DIR << std::endl;
+    
     for (int i = 1; i < argc; ++i) {
         std::cout << "Input[" << i << "]: " << argv[i] << std::endl;
     }
