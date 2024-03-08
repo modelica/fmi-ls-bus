bf1037751c29a17341972cc43ef415ecaaafea9a
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 13:46:08 +0100

    Correct syntax

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -3,8 +3,7 @@
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
-    std::cout << "Running tests in directory: " << TEST_DIR << std::endl;
-
+    std::cout << "Running tests in directory: '" << TEST_DIR << "'" << std::endl;
     for (int i = 1; i < argc; ++i) {
         std::cout << "Input[" << i << "]: " << argv[i] << std::endl;
     }
