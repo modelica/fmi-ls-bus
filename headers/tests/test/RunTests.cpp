2c532bc76fa874d56d67caf8a091c1244a2939da
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-08 11:55:11 +0100

    Undo try

--- headers/tests/test/RunTests.cpp
+++ headers/tests/test/RunTests.cpp
@@ -5,7 +5,6 @@ int main(int argc, char** argv) {
 
     std::cout << "TEST_DIR '" << TEST_DIR << "'" << std::endl;
     for (int i = 1; i < argc; ++i) {
-        std::cout << "HALLOHALLOHALLOHALLO";
         std::cout << "Input[" << i << "]: " << argv[i] << std::endl;
     }
 
