251f397c2622243f5db197fe0435423dd45cbb43
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-14 14:09:54 +0100

    Addition to prior change

--- headers/tests/helper/hdr/fmi_3_ls_bus_header_test_helper_can.h
+++ headers/tests/helper/hdr/fmi_3_ls_bus_header_test_helper_can.h
@@ -25,7 +25,7 @@ enum Operation { CanTransmit, CanFdTransmit, CanXlTransmit, Confirm, Configurati
  * \param[in] data        The data of the CAN Transmit operation.
  * \param[in] correctData Indicator if the method checks for a match of provided and created values or for an overflow of values.
  */
-void CheckCanTransmitOperation(long long int id, int ide, int rtr, fmi3UInt8 data[], bool correctData);
+void CheckCanTransmitOperation(long long int id, int ide, int rtr, int dataSize, fmi3UInt8 data[], bool correctData);
 
 /**
  * \brief Checks if the CAN FD Transmit operation is created correctly by the corresponding macro.
@@ -37,7 +37,7 @@ void CheckCanTransmitOperation(long long int id, int ide, int rtr, fmi3UInt8 dat
  * \param[in] data        The data of the CAN FD Transmit operation.
  * \param[in] correctData Indicator if the method checks for a match of provided and created values or for an overflow of values.
  */
-void CheckCanFdTransmitOperation(long long int id, int ide, int brs, int esi, fmi3UInt8 data[], bool correctData);
+void CheckCanFdTransmitOperation(long long int id, int ide, int brs, int esi, int dataSize, fmi3UInt8 data[], bool correctData);
 
 /**
  * \brief Checks if the CAN XL Transmit operation is created correctly by the corresponding macro.
@@ -51,7 +51,7 @@ void CheckCanFdTransmitOperation(long long int id, int ide, int brs, int esi, fm
  * \param[in] data        The data of the CAN XL Transmit operation.
  * \param[in] correctData Indicator if the method checks for a match of provided and created values or for an overflow of values.
  */
-void CheckCanXlTransmitOperation(long long int id, int ide, int sec, int sdt, int vcid, long long int af, fmi3UInt8 data[], bool correctData);
+void CheckCanXlTransmitOperation(long long int id, int ide, int sec, int sdt, int vcid, long long int af, int dataSize, fmi3UInt8 data[], bool correctData);
 
 /**
  * \brief Checks if the Confirm operation is created correctly by the corresponding macro.
