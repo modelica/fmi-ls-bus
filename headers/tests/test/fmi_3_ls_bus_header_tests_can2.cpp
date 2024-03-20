aa52f4e959b8d7862e95f9c2c839b9371fea4d89
emvahle <161011562+emvahle@users.noreply.github.com>
2024-03-20 10:41:29 +0100

    Combining workflows (#39)
    * Combining workflows
    
    * Add dummy file
    
    * Fix branch name
    
    * Add debug output
    
    * Fix debug output
    
    * Remove dummy output
    
    * Add dummy module
    
    * Adjust makefile for dummy module
    
    * Rename and remove combined workflows
    
    * Add main branch to trigger

---
+++ headers/tests/test/fmi_3_ls_bus_header_tests_can2.cpp
@@ -0,0 +1,36 @@
+#include "fmi_3_ls_bus_header_test_helper_can2.h"
+#include <gtest/gtest.h>
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4309)
+#pragma warning(disable : 4305)
+#endif
+
+/**
+ * \brief Test for the CAN Transmit operation with smallest valid values.
+ */
+TEST(Fmi3LsBusCanTransmit2, minValues2) {
+
+	fmi3UInt8 data[] = { 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D' };
+	CheckCanTransmitOperation2(0, 0, 0, data, true);
+}
+
+
+/**
+ * \brief Test for the CAN Transmit operation with biggest valid values.
+ */
+TEST(Fmi3LsBusCanTransmit2, maxValues2) {
+
+	fmi3UInt8 data[] = { 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D' };
+	CheckCanTransmitOperation2(UINT32_MAX, UINT8_MAX, UINT8_MAX, data, true);
+}
+
+/**
+ * \brief Test for the CAN Transmit operation with values that are too big.
+ */
+TEST(Fmi3LsBusCanTransmit2, wrongValues2) {
+
+	fmi3UInt8 data[] = { 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D' };
+	CheckCanTransmitOperation2(WRONG_U_INT322, WRONG_U_INT82, WRONG_U_INT82, data, false);
+}
